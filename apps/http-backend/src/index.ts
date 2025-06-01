e745a3deb0b32eff1bb1322c859546bb39638804
Mathura <Torque-Lab@users.noreply.github.com>
2025-06-01 13:22:43 +0530

    added backend common

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -21,9 +21,6 @@ app.post("/website", async (req, res) => {
   });
 });
 
-app.get("/status/:websiteId", (req, res) => {
-
-  
-});
+app.get("/status/:websiteId", (req, res) => {});
 
 app.listen(process.env.PORT || 3001);
