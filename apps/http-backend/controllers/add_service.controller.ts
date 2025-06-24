0b4f1820a85f14fa8fec524d5030b29b49459a0d
Mathura <Torque-Lab@users.noreply.github.com>
2025-06-24 15:20:41 +0530

    added endpoint to create service

---
+++ apps/http-backend/controllers/add_service.controller.ts
@@ -0,0 +1,28 @@
+import { ServiceSchema } from "../zod/serviceSchema";
+import {Request, Response} from "express"
+import {prismaClient} from "@repo/db/prisma"
+
+export const createService=async(req:Request,res:Response)=>{
+  
+      try {
+        const parsedData = ServiceSchema.safeParse(req.body);
+           if(!parsedData.success) {
+             res.status(400).json({ error: "Invalid data" });
+             return;
+           }
+           const {serviceName,url ,email}=parsedData.data
+           const  user_id=req.user!
+           const website=await prismaClient.website.create({
+            data:{
+                serviceName,
+                url,
+                email,
+                user_id
+            }
+           })
+           res.status(201).json({ message: "Website created successfully",serviceId:website.id });   
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Failed to create website" });
+      }
+}
