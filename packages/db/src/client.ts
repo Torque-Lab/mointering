1c680e53d604770ec55e72c55c0a9c795f1a42a0
Mathura <Torque-Lab@users.noreply.github.com>
2025-07-05 18:34:18 +0530

    fixe error

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient ,WebsiteStatus,Website,WebsiteTick} from "../generated/prisma/index.js";
+import { PrismaClient ,WebsiteStatus} from "../generated/prisma/index.js";
 
 export const prismaClient = new PrismaClient();
 export const websiteStatus=WebsiteStatus;
