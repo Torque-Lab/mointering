15bbfc6f738d69dd1b2c4018e826b133c585a5ab
Mathura <Torque-Lab@users.noreply.github.com>
2025-07-06 18:21:52 +0530

    fixed docker cmd error

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -2,17 +2,22 @@
 import process from 'process';
 const apiBaseURL = process.env.NEXT_PUBLIC_API_BASE_URL; 
 const isDev = process.env.NEXT_PUBLIC_ENV === 'development';
+
 const nextConfig = {
-    //This proxy is only efficient in development mode, 
+    // Enable standalone output for efficient docker build
+    output: 'standalone',
+    
+    // This proxy is only efficient in development mode
     ...(isDev && {
-        async rewrites(){
-            return[
+        async rewrites() {
+            return [
                 {
                     source: '/api/:path*',
                     destination: `${apiBaseURL}/api/:path*`,
                 }
-        ]
-    }})      
+            ];
+        }
+    })
 };
 
 export default nextConfig;
